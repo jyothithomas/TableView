b646e532413687273be653c96129f454e7a5aeb7
Avani Patel <anpatel36@yahoo.in>
2020-03-04 20:18:04 -0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Avani Patel on 3/4/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
